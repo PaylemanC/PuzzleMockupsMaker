ef2286accd67a48e24f527406ef3b43d02bf84c4
Carina Payleman <rociocarina2@gmail.com>
2025-02-28 17:45:27 -0300

    Create components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MockupComponent } from './components/mockup/mockup.component';
+import { MockupsListComponent } from './components/mockups-list/mockups-list.component';
+import { MockupsCardComponent } from './components/mockups-card/mockups-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MockupComponent,
+    MockupsListComponent,
+    MockupsCardComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/mockups-card/mockups-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mockups-card',
+  templateUrl: './mockups-card.component.html',
+  styleUrls: ['./mockups-card.component.scss']
+})
+export class MockupsCardComponent {
+
+}
