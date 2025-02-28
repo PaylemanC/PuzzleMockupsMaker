6e95a1e8bb7989444103de16ae082bbf69c6e2ad
Carina Payleman <rociocarina2@gmail.com>
2025-02-28 17:59:13 -0300

    Define first routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MockupsListComponent } from './components/mockups-list/mockups-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: MockupsListComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
