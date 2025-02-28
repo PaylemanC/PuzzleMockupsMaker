3652513e35333faaec612205ebfdfa6cdb3e1fc3
Carina Payleman <rociocarina2@gmail.com>
2025-02-28 18:23:44 -0300

    Create second routing to mockup component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MockupsListComponent } from './components/mockups-list/mockups-list.component';
+import { MockupComponent } from './components/mockup/mockup.component';
 
 const routes: Routes = [
-  { path: '', component: MockupsListComponent }
+  { path: '', component: MockupsListComponent },
+  { path: 'mockup/:id', component: MockupComponent }
 ];
 
 @NgModule({
--- src/app/components/mockup/mockup.component.ts
+++ src/app/components/mockup/mockup.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Mockup } from 'src/app/models/mockup.model';
+import { MOCKUPS } from 'src/app/data/mockups-list.data';
 
 @Component({
   selector: 'app-mockup',
@@ -6,5 +9,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./mockup.component.scss']
 })
 export class MockupComponent {
+  mockup: Mockup | undefined;
 
+  constructor(private route: ActivatedRoute) { }
+
+  ngOnInit() {
+    const id = this.route.snapshot.paramMap.get('id');
+    this.mockup = MOCKUPS.find(mockup => mockup.id === id);
+  }
 }
