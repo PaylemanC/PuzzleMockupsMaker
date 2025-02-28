24f11c371e2f33e726a390523a68e64a9d452133
Carina Payleman <rociocarina2@gmail.com>
2025-02-28 18:13:24 -0300

    Create mockups-list/card HTML/SCSS structure and CSS reset

--- src/app/components/mockups-card/mockups-card.component.ts
+++ src/app/components/mockups-card/mockups-card.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Input } from '@angular/core';
+import { Mockup } from 'src/app/models/mockup.model';
 
 @Component({
   selector: 'app-mockups-card',
@@ -6,5 +8,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./mockups-card.component.scss']
 })
 export class MockupsCardComponent {
-
+  @Input() mockup!: Mockup;
 }
--- src/app/components/mockups-list/mockups-list.component.ts
+++ src/app/components/mockups-list/mockups-list.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { Mockup } from 'src/app/models/mockup.model';
+import { MOCKUPS } from 'src/app/data/mockups-list.data';
 
 @Component({
   selector: 'app-mockups-list',
-  template: '<p>mockups-list works!</p>',
+  template: `<div id="mockups-list">
+  <p class="mockups-title">Escoge tu mockup:</p>
+  <div class="mockups-container">
+    <app-mockups-card *ngFor="let mockup of mockups" [mockup]="mockup"></app-mockups-card>
+  </div>
+</div>`,
   styleUrls: ['./mockups-list.component.scss']
 })
 export class MockupsListComponent {
-
+  mockups: Mockup[] = MOCKUPS;
 }
